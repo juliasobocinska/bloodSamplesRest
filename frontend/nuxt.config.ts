c2af0d9c1b2d0384ca961dc2a62b9447a3fdb4e0
Kimimate <103746985+Kimimate@users.noreply.github.com>
2026-05-31 20:24:29 +0200

    feat: add cors dependency and configure devServer port in nuxt.config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,28 +1,34 @@
+/* eslint-disable */
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui'
   ],
-
   devtools: {
     enabled: true
   },
-
   css: ['~/assets/css/main.css'],
-
   routeRules: {
     '/': { prerender: true }
   },
-
   compatibilityDate: '2025-01-15',
-
   eslint: {
     config: {
       stylistic: {
         commaDangle: 'never',
         braceStyle: '1tbs'
       }
+    }
+  },
+  // 1. port frontendu
+  devServer: {
+    port: 3001
+  },
+  // 2. port beckendu
+  runtimeConfig: {
+    public: {
+      apiBase: 'http://localhost:3000'
     }
   }
 })
